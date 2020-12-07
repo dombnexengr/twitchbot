e879906bb582d49fdc63206a2e102951b61386f0
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 03:26:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,7 @@ var config = {
     identity: {
         username: "dombnexen",
         // get yours at http://twitchapps.com/tmi
-        //password: "oauth:drcpv4njerm9wrv48j04590pztvel3" //client.login(process.env.password);//password secret for twitch
-        client.login(process.env.password);
+        password: "oauth:drcpv4njerm9wrv48j04590pztvel3" //client.login(process.env.password);//password secret for twitch
     },
     channels: [channel]
 }
