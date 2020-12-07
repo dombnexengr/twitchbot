06950aecb0c5196343a588a0992611358c23a030
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 02:26:13 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+var tmi = require("tmi.js")
+var channel = "dombnexen"
+
+var config = {
+    options: {
+        debug: true
+    }, 
+    connection: {
+        cluster: "aws", 
+        reconnect: true
+    },
+    identity: {
+        username: "dombnexen",
+        // get yours at http://twitchapps.com/tmi
+        //password: "oauth:aaftg53zgt0l9scxd6xn9ltzbavez0"
+        client.login(process.env.password);//password secret for twitch
+    },
+    channels: ["dombnexen"]
+}
+
+var client = new tmi.client(config)
+client.connect();
+
+client.on("connected", (address, port) => {
+    client.action(channel, "The bot has connected!")
+})
+
+client.on("chat", (channel, user, message, self) => {
+    if (self) return;
+    if (message == "hi") {
+        client.say(channel, "hey pal.")
+    }
+})
