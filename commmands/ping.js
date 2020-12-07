46aa06159c169311e277796ef367fb16477c4e17
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 05:35:09 +0200

    Delete ping.js

--- commmands/ping.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, message, args, user, channel, self) => {
-    client.ping().then(function(data) {
-        let ping = Math.floor(Math.round(data*1000))
-        client.say(channel, `@${user.username}, your ping is ${ping}`)
-    })
-}
