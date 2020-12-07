0ffbedd5fe211d69b00e593ce011c9547100819b
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 03:52:16 +0200

    Update discord.js

--- commmands/discord.js
+++ commmands/discord.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args, user, channel, self) => {
     client.discord().then(function(data) {
+        let discord = Math.floor(Math.round(data*1000))
         client.say(channel, `@${user.username}, Our Discord is https://discord.gg/mM7cUja`)
     })
 }
